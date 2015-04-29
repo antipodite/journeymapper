50908c9cccf91d9259770875b4387c6a37bb9d64
Isaac Stead <isaac.stead@protonmail.com>
2015-04-29 15:17:32 +1200

    Updated DB URL to use environment variable

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True # Cross-site request forgery protection for Flask-WTF
     SECRET_KEY = "nv+v9*+#th$vSZgM_F&7rV%nP+wNfw8e"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/boswell_dev"
 
 class ProductionConfig(Config):
     pass
