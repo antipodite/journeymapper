a6c805f7993c7d6d6b350b6630d8e6bc0a441db4
Isaac Stead <isaac.stead@protonmail.com>
2015-04-29 22:42:55 +1200

    Added AlchemyDumps to allow dumping DB data.
    Will put dump files under version control also. Hopefully this will be a good
    way to transfer data from the development database to the production database
    on Heroku... I'll try it out.

--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
 import os
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.alchemydumps import AlchemyDumps, AlchemyDumpsCommand
 from app import app, db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+# Flask-Migrate support
 migrate = Migrate(app, db)
 manager = Manager(app)
-
 manager.add_command('db', MigrateCommand)
+
+# AlchemyDumps support
+alchemydumps = AlchemyDumps(app, db)
+manager.add_command('dump', AlchemyDumpsCommand)
 
 if __name__ == '__main__':
     manager.run()
