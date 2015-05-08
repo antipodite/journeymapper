655e05b5c81bb7a87fd6af795df43ea6ed6eec95
Isaac Stead <isaac.stead@protonmail.com>
2015-05-08 21:44:17 +1200

    Partially refactored map interface - all map functionality now in MapViewer obj

--- static/mapinterface.js
+++ static/mapinterface.js
@@ -1,3 +1,4 @@
+/* Helper functions */
 // Maths to make bearing arrows work, and maybe other cool stuff I haven't
 // thought of yet, who knows?
 function degreesToRadians (degrees) {
@@ -23,7 +24,6 @@ function findBearing (latlng1, latlng2) {
     return radiansToDegrees(bearing);
 }
 
-
 function objectValues(obj) {
     var vals = Object.keys(obj).map(function(key) {
         return obj[key];
@@ -31,129 +31,153 @@ function objectValues(obj) {
     return vals;
 }
 
-// This is wrapped in a self-executing function which returns the actual
-// function so I can have private scope to hold the previousClickedMarker var.
-var addMarker = (function() {
+var MapViewer = (function () {
 
-    // This is used to save a reference to the previous clicked marker, so I
-    // can set it back to its unhighlighted state.
-    var previousClickedMarker;
+    var map;
+    var markers = [];
 
-    // This is used to save a reference to the previous created marker for
-    // the bearing calculation.
-    var previousCreatedMarkerPos;
+    var initialise = function (elem) {
+        var options = {
+            center: { lat: -50.7, lng: 166.1},
+            zoom: 6,
+            mapTypeId: 'satellite'
+        };
 
-    // The unhighlighted appearance of the markers
-    var defaultIcon = {
-        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        scale: 4,
-        strokeColor: 'red',
-        strokeWeight: 1,
+        map = new google.maps.Map(elem, options);
 
-        setRotation: function (degrees) {
-            this.rotation = degrees;
-        }
-    }
-
-    // The highlighted appearance of the markers
-    var focusIcon = {
-        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        scale: 6,
-        strokeColor: 'yellow',
-        strokeWeight: 1,
-
-        setRotation: function (degrees) {
-            this.rotation = degrees;
-        }
-    }
-
-    return function(map, id, latlng) {
-
-        // Calculate the bearing _from_ the previous marker _to_ this marker
-        // If no previous marker, circle instead of arrow?
-        var bearing = 0;
-        if (previousCreatedMarkerPos) {
-            bearing = findBearing(previousCreatedMarkerPos, latlng);
-        }
-
-        var marker = new google.maps.Marker({
-            position: latlng,
-            map: map,
-            entry_id: id,
-            icon: defaultIcon,
-
-            setHighlight: function() {
-                this.setIcon(focusIcon);
-            },
-
-            unsetHighlight: function() {
-                this.setIcon(defaultIcon);
-            }
-        });
-        marker.icon.setRotation(bearing);
-
-        // We need to calculate the bearing between this marker and the previous
-        // marker, so we need to remember what the previous created marker's
-        // position is
-        previousCreatedMarkerPos = latlng;
-
-        // The URL to query server for the next 10 entries
-        var url = '/entries/query/?count=10&eid=' + marker.entry_id;
-
-        // Define the behaviour for when user clicks on a marker
-        google.maps.event.addListener(marker, 'click', function() {
-            // Unset the highlight on the previous marker, if there is a
-            // previous marker
-            if (previousClickedMarker) {
-                previousClickedMarker.unsetHighlight();
-            }
-            // Change the colour of the marker to highlight it
-            marker.setHighlight();
-            $.get(url, function(response) {
-                var viewer = $('#entry_box');
-                viewer.empty();
-                viewer.append(response);
-                viewer.animate({scrollTop: 0});
+        $.getJSON('/all-positions', function(response) {
+            // Center the map on the first entry received
+            first = Object.keys(response)[0];
+            map.setCenter(response[first]);
+            // Create the location markers
+            $.each(response, function(id, latlng) {
+                var m = addMarker(id, latlng);
+                markers.push(m);
             });
-            previousClickedMarker = marker;
+
+            // Draw a line between the markers
+            var line = new google.maps.Polyline({
+                path: objectValues(response),
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 1
+            });
+            line.setMap(map);
         });
+    };
+
+    var addMarker = (function() {
+        // Save previous clicked marker for unhighlight
+        var previousClickedMarker;
+
+        // Save previously created marker during initialisation for bearing
+        var previousCreatedMarkerPos;
+
+        // The unhighlighted appearance of the markers
+        var defaultIcon = {
+            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+            scale: 4,
+            strokeColor: 'red',
+            strokeWeight: 1,
+
+            setRotation: function (degrees) {
+                this.rotation = degrees;
+            }
+        }
+
+        // The highlighted appearance of the markers
+        var focusIcon = {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 6,
+            strokeColor: 'yellow',
+            strokeWeight: 3,
+
+            setRotation: function (degrees) {
+                this.rotation = degrees;
+            }
+        }
+
+        return function(id, latlng) {
+
+            // Calculate the bearing _from_ the previous marker _to_ this marker
+            // If no previous marker, circle instead of arrow?
+            var bearing = 0;
+            if (previousCreatedMarkerPos) {
+                bearing = findBearing(previousCreatedMarkerPos, latlng);
+            }
+
+            var marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+                entry_id: id,
+                icon: defaultIcon,
+
+                setHighlight: function() {
+                    this.setIcon(focusIcon);
+                },
+
+                unsetHighlight: function() {
+                    this.setIcon(defaultIcon);
+                }
+            });
+            marker.icon.setRotation(bearing);
+
+            // Remember this marker for bearing calculation
+            previousCreatedMarkerPos = latlng;
+
+            // The URL to query server for the next 10 entries
+            var url = '/entries/query/?count=10&eid=' + marker.entry_id;
+
+            // Define the behaviour for when user clicks on a marker
+            google.maps.event.addListener(marker, 'click', function() {
+                // Unset the highlight on the previous marker, if there is a
+                // previous marker
+                if (previousClickedMarker) {
+                    previousClickedMarker.unsetHighlight();
+                }
+                // Change the colour of the marker to highlight it
+                marker.setHighlight();
+                $.get(url, function(response) {
+                    var viewer = $('#entry_box');
+                    viewer.empty();
+                    viewer.append(response);
+                    viewer.animate({scrollTop: 0});
+                });
+                previousClickedMarker = marker;
+            });
+            return marker;
+        };
+    })();
+
+    // Public API
+    return {
+        initialise: initialise
     };
 })();
 
 function initialise()
 {
-    var options = {
-        // This should be centered on the first location marker
-        center: { lat: -50.7, lng: 166.1},
-        zoom: 6,
-        mapTypeId: 'satellite'
-    };
 
-    var map = new google.maps.Map(document.getElementById('map_box'), options);
-
-    $.getJSON('/all-positions', function(response) {
-        // Center the map on the first entry received
-        first = Object.keys(response)[0];
-        map.setCenter(response[first]);
-        // Create the location markers
-        $.each(response, function(id, latlng) {
-            addMarker(map, id, latlng);
-        });
-        // Draw a line between the markers
-        var line = new google.maps.Polyline({
-            path: objectValues(response),
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 1
-        });
-        line.setMap(map);
+    // Hide the markers when the zoom level is higher than a certain level to
+    // make the map look less busy
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+        var currentZoom = map.getZoom();
+        console.log(currentZoom);
+        if (currentZoom < 5) {
+            // Need access to the markers...
+            console.log('Change markers');
+        }
     });
 }
 
-google.maps.event.addDomListener(window, 'load', initialise);
 
 $(document).ready(function () {
+
+    var mapViewer = MapViewer;
+    var mapViewerElem = document.getElementById('map_box');
+    mapViewer.initialise(mapViewerElem);
+
     // Fill the entry viewer with the first ten entries
     var url = '/entries/query/?count=10';
     $.get(url, function(response) {
