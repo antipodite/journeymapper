bd8e98285559a6719fc4c9a36f89a7b590263cab
Isaac Stead <isaac.stead@protonmail.com>
2018-03-26 14:16:47 +1300

    Ported frontend code to CesiumJS.

---
+++ static/map.js
@@ -0,0 +1,38 @@
+var viewer = new Cesium.Viewer('map', {
+	baseLayerPicker : false,
+	infoBox : true,
+	timeline : false,
+	animation : false,
+	selectionIndicator : false
+});
+
+var pinBuilder = new Cesium.PinBuilder();
+
+createJournalEntryMarkers(viewer, pinBuilder);
+
+var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+handler.setInputAction(function(click) {
+	var pickedMarker = viewer.scene.pick(click.position);
+	if (Cesium.defined(pickedMarker)) {
+		var url = '/entry-text/' + pickedMarker.id.name;
+		$.getJSON(url, function(response) {
+			pickedMarker.id.description = response.text;
+		});
+	}
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+function createJournalEntryMarkers(viewer, pinBuilder) {
+	$.getJSON('/all-positions', function(response) {
+		$.each(response, function(id, latlng) {
+			viewer.entities.add({
+				name : id,
+				position : Cesium.Cartesian3.fromDegrees(latlng['lng'], latlng['lat']),
+				billboard : {
+					image : pinBuilder.fromColor(Cesium.Color.RED, 48),
+					verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+					scaleByDistance : new Cesium.NearFarScalar(2.0e2, 2.0, 8.0e6, 0.2)
+				}
+			})
+		});
+	});
+}
