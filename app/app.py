c12d6234ba9c8c25591cfee86a038fd9ce0957e0
Isaac Stead <isaac.stead@protonmail.com>
2018-04-15 18:11:03 +1200

    Implemented basic login functionality, plus default admin account

--- app/app.py
+++ app/app.py
@@ -2,9 +2,15 @@ import os
 import sys
 
 from datetime import datetime
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, flash, abort, redirect, url_for
+
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+
+from flask_login import LoginManager, login_user, logout_user
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 from app.config import DevConfig, LiveConfig
 from database.models import Journal, Entry, Metadata
@@ -26,16 +32,59 @@ db = SQLAlchemy(app, metadata=Metadata)
 db.create_all()
 session = db.session
 
+# Create admin user if it doesn't exist. TODO: Find better way of doing this 
+# that doesn't need password in source code
+if not db.session.query(User).filter_by(name='admin').first():
+    admin_user = User(name='admin', email='admin@navigato.rs')
+    admin_user.set_password('password')
+    db.session.add(admin_user)
+    db.session.commit()
+
 ## User login support
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign in')
+
+@login_manager.user_loader
+def load_user(userid):
+    return session.query(User).get(userid)
 
 ## Views
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        # TODO flashing messages requires support in template
+        try:
+            user = session.query(User).filter_by(name=username).first()
+            login_user(user)
+            flash('Logged in successfully.')
+            next = request.args.get('next')
+            #if not is_safe_url(next):
+            #    return abort(400)
+            return redirect(next or url_for('index'))
+        except AttributeError:
+            flash('No user named ' + username)
+
+    return render_template('login.html', title='Log in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/journal/<jid>/render')
 def render_journal_data(jid):
